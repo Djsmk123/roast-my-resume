b46b7d52db4fc948a3a75b6df62556e7995b874a
MD Mobin <74850678+Djsmk123@users.noreply.github.com>
2024-07-01 18:56:19 +0530

    Update route.ts

--- src/app/api/roast/route.ts
+++ src/app/api/roast/route.ts
@@ -13,8 +13,8 @@ enum tones {
     "soft-hearted",
     "hard-hearted",
     "light",
-    "soft-dark",
     "dark",
+    "vulgar",
 
 }
 const safetySettings = [{ category: HarmCategory.HARM_CATEGORY_HARASSMENT, threshold: HarmBlockThreshold.BLOCK_NONE, }, { category: HarmCategory.HARM_CATEGORY_HATE_SPEECH, threshold: HarmBlockThreshold.BLOCK_NONE, }, { category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT, threshold: HarmBlockThreshold.BLOCK_NONE, }, { category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT, threshold: HarmBlockThreshold.BLOCK_NONE, },];
