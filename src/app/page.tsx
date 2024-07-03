68b7b8e79262cb1acd3421085c67faebba4f4c95
djsmk123 <djsmk123@gmail.com>
2024-07-03 14:02:41 +0530

    chore: Update npm dependencies, including @vercel/analytics to version 1.3.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { FaGithub } from 'react-icons/fa';
 
 import { ClipLoader } from "react-spinners";
+import { Analytics } from "@vercel/analytics/react";
 
 enum RoastLevel {
   soft = "Soft-hearted",
@@ -130,6 +131,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
       <div className="w-full max-w-5xl p-8">
+        <Analytics />
         <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         </div>
 
